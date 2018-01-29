a567ac120d2afe935ba5f9ed1077f81ba15d34ca
Justin Lee <justinlee0022@gmail.com>
2018-01-29 13:35:26 -0800

    Refactored and wrote script that generates array of student emails for specified course

---
+++ get.js
@@ -0,0 +1,46 @@
+const Promise = require('bluebird')
+const request = require('request-promise')
+const linkparser = require('parse-link-header')
+const R = require('ramda')
+require('dotenv').config()
+
+const token = process.env.CANVAS_API_TOKEN
+const canvasInstance = process.env.CANVAS_INSTANCE
+
+const fetchAll = (url, result = []) =>
+  request({
+    'method': 'GET',
+    'uri': url,
+    'json': true,
+    'resolveWithFullResponse': true,
+    'headers': {
+      'Authorization': 'Bearer ' + token
+    }
+  }).then(response => {
+    result = [...result, ...response.body]
+    const links = linkparser(response.headers.link)
+    return links.next ? fetchAll(links.next.url, result) : result
+  })
+
+const getAllSubaccounts = accountId => fetchAll(canvasInstance + `/api/v1/accounts/${accountId}/sub_accounts?`)
+
+const getAllCourses = (deptIds, ...options) =>
+  Promise.all(deptIds.map(deptId => fetchAll(canvasInstance + `/api/v1/accounts/${deptId}/courses?`) + options.join('&')))
+
+const getAllStudents = (courseIds, ...options) =>
+  Promise.all(courseIds.map(courseId => fetchAll(canvasInstance + `/api/v1/courses/${courseId}/users?` + options.join('&'))))
+
+const getAllEmailsInCourse = (accountId, deptName, courseCode) => getAllSubaccounts(accountId)
+  .then(depts => depts.map(({ id, name }) => ({ id, name })))
+  .then(depts => depts.find(dept => dept.name === deptName))
+  .then(dept => getAllCourses([dept.id]))
+  .then(courses => R.flatten(courses))
+  .then(courses => courses.find(course => course.course_code === courseCode))
+  .then(course => getAllStudents([course.id], 'include[]=email'))
+  .then(students => R.flatten(students))
+  .then(students => students.map(student => student.email))
+
+module.exports.getAllSubaccounts = getAllSubaccounts
+module.exports.getAllCourses = getAllCourses
+module.exports.getAllStudents = getAllStudents
+module.exports.getAllEmailsInCourse = getAllEmailsInCourse
---
+++ index.js
@@ -0,0 +1,14 @@
+const Bottleneck = require('bottleneck')
+const { getAllSubaccounts, getAllCourses, getAllStudents, getAllEmailsInCourse } = require('./get.js')
+
+require('dotenv').config()
+const accountId = process.env.ACCOUNT_ID
+
+const limiter = new Bottleneck({
+  maxConcurrent: 20,
+  minTime: 100,
+  Promise: Promise
+})
+
+limiter.schedule(() =>
+  getAllEmailsInCourse(accountId, 'APBI', 'APBI200').then(emails => console.log(emails.length, emails)))

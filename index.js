87aacb48c0d2e1c44d1bf6389828e525ffcc249b
Justin Lee <justinlee0022@gmail.com>
2018-01-31 20:56:06 -0800

    Converted promises into async/await

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ const limiter = new Bottleneck({
   Promise: Promise
 })
 
-limiter.schedule(() => get.getUsersInCourse(3040, options.users.include.email)
-  .then(x => console.log(x))
+const getAllEmailsInCourse = async (accountId, deptName, courseCode) => {
+  const subAccounts = await get.getSubaccounts(accountId)
+  const depts = await subAccounts.map(({ id, name }) => ({ id, name }))
+  const dept = await depts.find(dept => dept.name === deptName)
+  const allCoursesInDept = await get.getCourses(dept.id)
+  const course = await allCoursesInDept.find(course => course.course_code === courseCode)
+  const students = await get.getUsersInCourse(course.id, options.users.include.email)
+  const emails = await students.map(student => student.email)
+  return emails
+}
+
+limiter.schedule(() => getAllEmailsInCourse(15, 'FNH', 'FNH250')
+  .then(emails => console.log(emails))
 )

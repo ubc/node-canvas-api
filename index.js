e0ef79773a3cfe7b01df694bf48ce267ab0349c1
Justin Lee <justinlee0022@gmail.com>
2018-02-02 10:47:41 -0800

    Added code to get student emails, names, combinations, and added unit test for buildOption

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Bottleneck = require('bottleneck')
 const putStudentNumbersInGradebook = require('./src/recipes/putStudentNumbersInGradebook')
 const deleteAllCustomGradebookColumns = require('./src/recipes/deleteAllCustomGradebookColumns')
+const { getStudentEmailsFromCourse, getStudentNamesFromCourse, getStudentNamesAndEmailsFromCourse, getStudentNumbersFromCourse, getStudentNumbersAndEmailsFromCourse } = require('./src/recipes/getStudentAttributesFromCourse')
+const get = require('./src/requests/get')
 
 const limiter = new Bottleneck({
   maxConcurrent: 20,
@@ -8,5 +10,8 @@ const limiter = new Bottleneck({
   Promise: Promise
 })
 
+// get.getUsersInCourse(3025).then(x => console.log(x))
+getStudentNumbersAndEmailsFromCourse(3025).then(x => console.log(x))
+
 // deleteAllCustomGradebookColumns(3025).then(x => console.log(x))
-putStudentNumbersInGradebook(3025, 'Student Number', 1)
+// putStudentNumbersInGradebook(3025, 'Student Number', 1)
---
+++ test/util.test.js
@@ -0,0 +1,14 @@
+/* global describe, it */
+const assert = require('assert')
+const { buildOptions } = require('../src/util')
+
+describe('buildOptions', () => {
+  it('returns an empty string if the array passed to it is empty', () => {
+    assert.deepEqual(buildOptions([]), '')
+  })
+  it('returns the options separated by &', () => {
+    const input = ['enrollment_type[]=teacher', 'include[]=locked', 'include[]=custom_links']
+    const output = 'enrollment_type[]=teacher&include[]=locked&include[]=custom_links'
+    assert.deepEqual(buildOptions(input), output)
+  })
+})

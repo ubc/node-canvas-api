6715c573975798c4b025888b3957f924baac24b0
Justin Lee <justinlee0022@gmail.com>
2018-02-03 08:37:09 -0800

    refactored putStudentNumbersInGradebook

--- src/recipes/putStudentNumbersInGradebook.js
+++ src/recipes/putStudentNumbersInGradebook.js
@@ -1,14 +1,14 @@
 const { getOptions } = require('../requests/options')
-const get = require('../requests/get')
-const post = require('../requests/post')
-const put = require('../requests/put')
+const { getUsersInCourse } = require('../requests/get')
+const { createCustomGradebookColumn } = require('../requests/post')
+const { putStudentNumberInGradeColumn } = require('../requests/put')
 
 const putStudentNumbersInGradebook = async (courseId, columnTitle, columnPosition) => {
-  const customGradeBook = await post.createCustomGradebookColumn(courseId, columnTitle, columnPosition)
+  const customGradeBook = await createCustomGradebookColumn(courseId, columnTitle, columnPosition)
   const customGradeBookId = await customGradeBook.id
-  const studentObjs = await get.getUsersInCourse(courseId, getOptions.users.enrollmentType.student)
+  const studentObjs = await getUsersInCourse(courseId, getOptions.users.enrollmentType.student)
   const responseCustomGradebook = Promise.all(studentObjs.map(({ id, sis_user_id }) =>
-    put.putStudentNumberInGradeColumn(courseId, customGradeBookId, id, { 'column_data[content]': sis_user_id })))
+    putStudentNumberInGradeColumn(courseId, customGradeBookId, id, { 'column_data[content]': sis_user_id })))
   return responseCustomGradebook
 }
 

4d7c93b10be2ec29beaa2beb61ec29f60a0218c4
Justin Lee <justin.lee@ubc.ca>
2018-02-13 18:26:17 -0800

    Simplified index.js for gradebook

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-const Bottleneck = require('bottleneck')
 const putStudentNumbersInGradebook = require('./src/recipes/putStudentNumbersInGradebook')
-const deleteAllCustomGradebookColumns = require('./src/recipes/deleteAllCustomGradebookColumns')
-const { getStudentEmailsFromCourse, getStudentNamesFromCourse, getStudentNamesAndEmailsFromCourse, getStudentNumbersFromCourse, getStudentNumbersAndEmailsFromCourse } = require('./src/recipes/getStudentAttributesFromCourse')
-const { getUsersInCourse } = require('./src/requests/get')
 
-const limiter = new Bottleneck({
-  maxConcurrent: 20,
-  minTime: 100,
-  Promise: Promise
-})
-
-getUsersInCourse(3025).then(x => console.log(x))
-// getStudentNumbersAndEmailsFromCourse(3025).then(x => console.log(x))
-
-// deleteAllCustomGradebookColumns(3025).then(x => console.log(x))
-// putStudentNumbersInGradebook(3025, 'Student Number', 1)
+// modify the CHANGE_ME to a Canvas course id.
+putStudentNumbersInGradebook(CHANGE_ME, 'Student Number', 1)

7534e6d5d851c0c2a649be0a7bebda23c9801c9e
Justin Lee <justinlee0022@gmail.com>
2018-02-14 17:54:50 -0800

    Added in option to only get actively enrolled students, as per Andrew's suggestion. Currently thinking of refactoring functions so that instead of taking infinite number of option parameters, I can pass an object. Not entirely sure which implementation is cleaner

--- src/recipes/getStudentAttributesFromCourse.js
+++ src/recipes/getStudentAttributesFromCourse.js
@@ -8,25 +8,25 @@ const getStudentNamesFromCourse = async courseId => {
 }
 
 const getStudentEmailsFromCourse = async courseId => {
-  const studentObjs = await getUsersInCourse(courseId, getOptions.users.enrollmentType.student, getOptions.users.include.email)
+  const studentObjs = await getUsersInCourse(courseId, getOptions.users.enrollmentType.student, getOptions.users.include.email, getOptions.users.enrollmentState.active)
   const studentEmails = await studentObjs.map(student => student.email)
   return studentEmails
 }
 
 const getStudentNumbersFromCourse = async courseId => {
-  const studentObjs = await getUsersInCourse(courseId, getOptions.users.enrollmentType.student)
+  const studentObjs = await getUsersInCourse(courseId, getOptions.users.enrollmentType.student, getOptions.users.enrollmentState.active)
   const studentNumbers = await studentObjs.map(student => student.sis_user_id)
   return studentNumbers
 }
 
 const getStudentNamesAndEmailsFromCourse = async courseId => {
-  const studentObjs = await getUsersInCourse(courseId, getOptions.users.enrollmentType.student, getOptions.users.include.email)
+  const studentObjs = await getUsersInCourse(courseId, getOptions.users.enrollmentType.student, getOptions.users.include.email, getOptions.users.enrollmentState.active)
   const studentNamesAndEmails = await studentObjs.map(({ id, email }) => ({ id, email }))
   return studentNamesAndEmails
 }
 
 const getStudentNumbersAndEmailsFromCourse = async courseId => {
-  const studentObjs = await getUsersInCourse(courseId, getOptions.users.enrollmentType.student, getOptions.users.include.email)
+  const studentObjs = await getUsersInCourse(courseId, getOptions.users.enrollmentType.student, getOptions.users.include.email, getOptions.users.enrollmentState.active)
   const studentNumbersAndEmails = await studentObjs.map(({ sis_user_id, email }) => ({ sis_user_id, email }))
   return studentNumbersAndEmails
 }

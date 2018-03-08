4839eabf15f916301b50f926d1f392eb120e7935
Justin Lee <justinlee0022@gmail.com>
2018-03-08 14:53:15 -0800

    Added getAllCoursesInAccounts recipe

---
+++ src/recipes/getAllCoursesInAccount.js
@@ -0,0 +1,13 @@
+const { getSubaccounts, getCourses } = require('../requests/get')
+const { getOptions } = require('../requests/options')
+const R = require('ramda')
+
+const getAllCoursesInAccount = async (accountId, ...options) => {
+  const accounts = await getSubaccounts(accountId)
+  const courses = await Promise.all(
+    accounts.map(({id}) => getCourses(id, ...options))
+  )
+  return R.flatten(courses)
+} 
+
+module.exports = getAllCoursesInAccount

2f2dcffe7773e1c62169d02746de45219ed59786
Justin Lee <justinlee0022@gmail.com>
2018-03-10 13:00:18 -0800

    working on progress

--- src/recipes/copyCourseContent.js
+++ src/recipes/copyCourseContent.js
@@ -1,8 +1,11 @@
 const { courseCopy } = require('../requests/post')
+const { getProgress } = require('../requests/get')
 
 const copyCourseMaterial = async (sourceCourseId, targetCourseId, options) => {
   const contentMigration = await courseCopy(sourceCourseId, targetCourseId)
-  return contentMigration
+  const progress = getProgress(contentMigration.id)
+  // maybe implement polling to see when the content migration is complete?
+  return progress
 }
 
 module.exports = copyCourseMaterial
--- src/requests/get.js
+++ src/requests/get.js
@@ -34,5 +34,7 @@ module.exports = {
   getCoursesByUser: (userId, ...options) =>
     fetchAll(canvasDomain + `/users/${userId}/courses`),
   getCustomGradeBookColumns: courseId =>
-    fetchAll(canvasDomain + `/courses/${courseId}/custom_gradebook_columns`)
+    fetchAll(canvasDomain + `/courses/${courseId}/custom_gradebook_columns`),
+  getProgress: id =>
+    fetchAll(canvasDomain + `/progress/${id}`)
 }

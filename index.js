45f7a1199c8e92910a5cd36ebd7a04c3c4fe97c9
Justin Lee <justinlee0022@gmail.com>
2018-03-10 14:16:25 -0800

    added getAllCoursesInAccount to index

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const recipes = require('./src/recipes/index')
 const requests = require('./src/requests/index')
 
-recipes.getAccountIds().then(x => console.log(x))
+recipes.getAccountIds()
+  .then(x => console.log(x))
--- src/recipes/index.js
+++ src/recipes/index.js
@@ -1,9 +1,10 @@
 module.exports = {
   copyCourseContent: require('./copyCourseContent'),
   deleteAllCustomGradebookColumns: require('./deleteAllCustomGradebookColumns'),
+  getAccountIds: require('./getAccountIds'),
+  getAllCoursesInAccount: require('./getAllCoursesInAccount'),
   getDeptIdsInAccount: require('./getDeptIdsInAccount'),
   getStudentAttributesFromCourse: require('./getStudentAttributesFromCourse'),
   putStudentNumbersInGradebook: require('./putStudentNumbersInGradebook'),
-  getAllCoursesInDept: require('./getAllCoursesInDept'),
-  getAccountIds: require('./getAccountIds')
+  getAllCoursesInDept: require('./getAllCoursesInDept')
 }

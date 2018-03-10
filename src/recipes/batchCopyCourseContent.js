7f9b3a82244e7cfe5d60f2525098be09ed3907ca
Justin Lee <justinlee0022@gmail.com>
2018-03-10 15:52:32 -0800

    made it look nicer

--- src/recipes/batchCopyCourseContent.js
+++ src/recipes/batchCopyCourseContent.js
@@ -2,7 +2,8 @@ const copyCourseContent = require('./copyCourseContent')
 
 const batchCopyCourseContent = async listOfSourceAndTargetCourseIds => {
   const listOfProgress = listOfSourceAndTargetCourseIds
-    .map(({ sourceCourseId, targetCourseId }) => copyCourseContent(sourceCourseId, targetCourseId))
+    .map(({ sourceCourseId, targetCourseId }) =>
+      copyCourseContent(sourceCourseId, targetCourseId))
   return listOfProgress
 }
 

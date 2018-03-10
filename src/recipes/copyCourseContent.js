00300e99ade51eac1c8af69261e9ab4210c62536
Justin Lee <justinlee0022@gmail.com>
2018-03-10 14:19:34 -0800

    removed options param from copyCourseMaterial

--- src/recipes/copyCourseContent.js
+++ src/recipes/copyCourseContent.js
@@ -1,7 +1,7 @@
 const { courseCopy } = require('../requests/post')
 const { getProgress } = require('../requests/get')
 
-const copyCourseMaterial = async (sourceCourseId, targetCourseId, options) => {
+const copyCourseMaterial = async (sourceCourseId, targetCourseId) => {
   const contentMigration = await courseCopy(sourceCourseId, targetCourseId)
   const progress = getProgress(contentMigration.id)
   // maybe implement polling to see when the content migration is complete?

2e1213b70e7d772ea5114159ac0063ac3cb0ed48
Justin Lee <justinlee0022@gmail.com>
2018-05-22 14:29:21 -0700

    Added to index the code necessary to first find the gradebook ID and then hide it

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const recipes = require('./src/recipes/index')
 const requests = require('./src/requests/index')
 const get = require('./src/requests/get')
+const put = require('./src/requests/put')
 const options = require('./src/options')
 
+const courseID = // put the course ID here
+const gradebookID = // put the gradebook ID heer
+
+get.getCustomGradeBookColumns(courseID)
+  .then(x => console.log(x))
+
+// put.hideCustomGradebookColumn(courseID, gradebookID)
+//   .then(x => console.log(x))

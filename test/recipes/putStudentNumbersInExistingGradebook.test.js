d0cf75757ba758f41d2d4bb69d65ae5ede097837
KevinCHWu <kevinwu0703@gmail.com>
2018-05-23 16:05:17 -0700

    Started Google Sheets API

--- test/recipes/putStudentNumbersInExistingGradebook.test.js
+++ test/recipes/putStudentNumbersInExistingGradebook.test.js
@@ -1,7 +1,27 @@
-/* global test, expect */
+/* global test, expect, describe, it, jest */
 
 const putStudentNumbersInExistingGradebook = require('../../src/recipes/putStudentNumberInExistingGradebook')
 
 test('putStudentNumbersInExistingGradebook function exists', () => {
   expect(typeof putStudentNumbersInExistingGradebook).toEqual('function')
+})
+
+// describe('putStudentNumbersInExistingGradebook', () => {
+//   it('Expect putStudentNumbersInExistingGradebook called with 2 args', () => {
+//     const putStudentNumberInGradeColumnMock = jest.fn()
+//     const putStudentNumbersInExistingGradebookMock = jest.fn(() => {
+//       putStudentNumberInGradeColumnMock()
+//     })
+
+//     expect(putStudentNumbersInExistingGradebookMock).toBeCalled()
+//     expect(putStudentNumberInGradeColumnMock).toBeCalled()
+//   })
+// })
+
+// work in progress
+test('Expect putStudentNumbersInExistingGradebook called with 2 args', () => {
+  expect.assertions(1)
+  const data = putStudentNumbersInExistingGradebook(2503)
+
+  return expect(data).toBeCalledWith()
 })

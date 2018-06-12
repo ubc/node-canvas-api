5782def59cebdfae7c9e771e79f948308c71d3ba
Justin Lee <justinlee0022@gmail.com>
2018-06-12 11:54:08 -0700

    Made code prettier

--- src/recipes/getAllCoursesWithNoSyllabus.js
+++ src/recipes/getAllCoursesWithNoSyllabus.js
@@ -4,7 +4,9 @@ const get = require('../requests/get')
 const getAllCoursesWithNoSyllabus = async (accountId, year, term) => {
   const courses = await getAllCoursesInTerm(accountId, year, term)
   const ids = courses.map(({ id }) => id)
-  const syllabi = await Promise.all(ids.map(id => get.getSyllabusOfCourse(id)))
+  const syllabi = await Promise.all(
+    ids.map(id => get.getSyllabusOfCourse(id))
+  )
   return syllabi.filter(x => x.syllabus === null)
 }
 

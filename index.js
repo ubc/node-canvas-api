1cec3ba2641e012c20169b089d209bab98b097c8
Justin Lee <justinlee0022@gmail.com>
2018-06-13 10:28:18 -0700

    Modified to be ready for export as NPM package

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const recipes = require('./src/recipes/index')
 const requests = require('./src/requests/index')
+const post = require('./src/requests/post')
 const get = require('./src/requests/get')
+const del = require('./src/requests/del')
 const put = require('./src/requests/put')
 const options = require('./src/options')
 
-recipes.getAllCoursesWithNoSyllabus(15, 2017, 'W2')
-  .then(x => console.log(x))
+module.exports = {
+  recipes,
+  requests,
+  get,
+  put,
+  options,
+  post,
+  del
+}

a588d41e14187f86bb1946eded4e4a77b26c54a4
Justin Lee <justinlee0022@gmail.com>
2018-06-14 10:28:52 -0700

    Updated config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,35 @@
-export default {
+import uglify from 'rollup-plugin-uglify'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import json from 'rollup-plugin-json'
+
+const config =  {
   input: 'index.js',
   output: {
     file: 'dist/node-canvas-api.js',
     format: 'umd',
-    exports: 'named',
     name: 'node-canvas-api'
-  }
+  },
+  plugins: [
+    resolve(),
+    commonjs({
+      include: 'node_modules/**'
+    }),
+    json()
+  ]
 }
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      }
+    })
+  )
+}
+
+export default config

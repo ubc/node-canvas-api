53df9b747c870402f63b3c1f215d9d92d6b60644
Justin Lee <justinlee0022@gmail.com>
2018-06-15 09:14:29 -0700

    Updated params

--- source/deleteAllCustomGradebookColumns.js
+++ source/deleteAllCustomGradebookColumns.js
@@ -2,7 +2,7 @@ import getCustomGradeBookColumns from './getCustomGradeBookColumns'
 import deleteCustomGradebookColumn from './deleteCustomGradebookColumn'
 
 /**
- * Creates course
+ * Deletes all custom gradebook columns
  * @param {Number} courseId the course ID of the target course.
  * @return {Promise} A list of CustomColumn objects: https://canvas.instructure.com/doc/api/custom_gradebook_columns.html#CustomColumn
  */

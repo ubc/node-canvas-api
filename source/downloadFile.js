8b9d468d6cfc14fc62f4d90fa33490542a2f6b18
Justin Lee <justinlee0022@gmail.com>
2018-06-26 10:40:36 -0700

    Added jsdoc for downloadFile

--- source/downloadFile.js
+++ source/downloadFile.js
@@ -16,6 +16,13 @@ const requestObj = url => ({
   }
 })
 
+/**
+ * Downloads specified fileID from Canvas to specified path
+ * @param {Number} fileId the fileId of the file
+ * @param {String} path the path that the file should be downloaded to
+ * @return {Promise} A Promise that resolves to a log that inidicated what the filename of the downloaded file is. On error, logs the error
+ */
+
 const downloadFile = (fileId, path) => getFile(fileId, path)
   .then(({ url, filename }) => {
     const pdfStream = fs.createWriteStream(path + filename)

396a690bb0c9aea91d337490f88fa0709a20633f
Justin Lee <justinlee0022@gmail.com>
2019-03-19 11:35:15 -0700

    Added more discussion endpoints

---
+++ src/getDiscussionTopic.js
@@ -0,0 +1,16 @@
+var fetch = require('./internal/fetch');
+
+const canvasDomain = process.env.CANVAS_API_DOMAIN;
+
+/**
+ * Retrives all discussion topics in course
+ * @param {Number} courseId the course id.
+ * @param {Number} topicId the discussion topic id.
+ * @return {Promise} A promise that resolves to a discussion topic Object
+ */
+
+const getDiscussionTopics = async (courseId, topicId) => {
+  return fetch(canvasDomain + `/courses/${courseId}/discussion_topics/${topicId}`);
+};
+
+module.exports = getDiscussionTopics;

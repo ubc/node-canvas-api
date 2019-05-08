26319605f7c79265c348091d4102a0251fc7b73e
Justin Lee <justinlee0022@gmail.com>
2019-05-08 10:27:44 -0700

    Fixed jsdoc for getAllCourseSyllabiInAccount

--- src/getAllCourseSyllabiInAccount.js
+++ src/getAllCourseSyllabiInAccount.js
@@ -3,11 +3,9 @@ var getAllCoursesInAccount = require('./getAllCoursesInAccount');
 var getSyllabusOfCourse = require('./getSyllabusOfCourse');
 
 /**
- * Returns the syllabi of all courses contained under an Account, for the specified year/term.
+ * Returns the syllabi of all courses contained under an Account
  * If a course does not have a syllabus defined, the syllabus_body will have null.
  * @param {Number} accountId the account ID for Canvas
- * @param {Number} year the year from which the syllabi should be pulled
- * @param {Number} term the term from which the syllabi should be pulled
  * @return {Promise} A promise that resolves to an array of course objects, with syllabus
  */
 

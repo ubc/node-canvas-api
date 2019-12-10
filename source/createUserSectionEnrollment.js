aa3bcc472768dce1658cab34470ea9bd3170e288
Wayne Porter <cwporter@ucsd.edu>
2019-12-10 13:48:38 -0800

    Add user enrollments

---
+++ source/createUserSectionEnrollment.js
@@ -0,0 +1,17 @@
+import postRequest from './internal/post'
+
+require('dotenv').config()
+
+const canvasDomain = process.env.CANVAS_API_DOMAIN
+
+/**
+ * Create a new user enrollment for section.
+ * @param {Number} sectionId the section ID the user is to be enrolled in.
+ * @param {Number} body the object that contains the request parameters as indicated here:
+ * https://canvas.instructure.com/doc/api/enrollments.html#method.enrollments_api.create
+ * @return {Promise} An Enrollment object: https://canvas.instructure.com/doc/api/enrollments.html#Enrollment
+ */
+
+ export default function createUserSectionEnrollment (sectionId, body) {
+	 return postRequest(canvasDomain + `/sections/${sectionId}/enrollments`, body)
+ }

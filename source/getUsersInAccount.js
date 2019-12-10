5843d7078255bd87453e78a9d4d8cd75e5c528c9
Wayne Porter <cwporter@ucsd.edu>
2019-12-10 14:14:45 -0800

    Fix some missing items

--- source/getUsersInAccount.js
+++ source/getUsersInAccount.js
@@ -8,6 +8,7 @@ const canvasDomain = process.env.CANVAS_API_DOMAIN
 /**
  * Retrieves all users under an Account
  * @param {Number} accountId the account id.
+ * @param {Array} options an array of options to include.
  * @return {Promise} A promise that resolves to a list of User objects: https://canvas.instructure.com/doc/api/users.html#User
  */
 

7398bf6755dd256784accc9c870b49d4fb8fd8cf
Wayne Porter <cwporter@ucsd.edu>
2019-12-12 10:18:54 -0800

    Prepare for using directly from github into npm

---
+++ src/createUserCourseEnrollment.js
@@ -0,0 +1,18 @@
+var postRequest = require('./internal/post');
+
+require('dotenv').config();
+
+const canvasDomain = process.env.CANVAS_API_DOMAIN;
+
+/**
+ * Create a new user enrollment for a course.
+ * @param {Number} courseId the course ID the user is to be enrolled in.
+ * @param {Number} body the object that contains the request parameters as indicated here:
+ * https://canvas.instructure.com/doc/api/enrollments.html#method.enrollments_api.create
+ * @return {Promise} An Enrollment object: https://canvas.instructure.com/doc/api/enrollments.html#Enrollment
+ */
+
+function createUserCourseEnrollment(courseId, body) {
+  return postRequest(canvasDomain + `/courses/${courseId}/enrollments`, body);
+}
+module.exports = createUserCourseEnrollment;
---
+++ src/createUserSectionEnrollment.js
@@ -0,0 +1,18 @@
+var postRequest = require('./internal/post');
+
+require('dotenv').config();
+
+const canvasDomain = process.env.CANVAS_API_DOMAIN;
+
+/**
+ * Create a new user enrollment for section.
+ * @param {Number} sectionId the section ID the user is to be enrolled in.
+ * @param {Number} body the object that contains the request parameters as indicated here:
+ * https://canvas.instructure.com/doc/api/enrollments.html#method.enrollments_api.create
+ * @return {Promise} An Enrollment object: https://canvas.instructure.com/doc/api/enrollments.html#Enrollment
+ */
+
+function createUserSectionEnrollment(sectionId, body) {
+  return postRequest(canvasDomain + `/sections/${sectionId}/enrollments`, body);
+}
+module.exports = createUserSectionEnrollment;

fed1bc82fa10ff610feb1a041cc8ce129dce82ec
Justin Lee <justin.lee@ubc.ca>
2020-10-22 08:17:58 -0700

    updated documentation

--- source/getCoursesByUser.js
+++ source/getCoursesByUser.js
@@ -7,7 +7,7 @@ const canvasDomain = process.env.CANVAS_API_DOMAIN
 /**
  * Get all courses a user is enrolled in
  * @param {Number} userId the user id.
- * @return {Promise} A promise that resolves to a User object: https://canvas.instructure.com/doc/api/users.html#User
+ * @return {Promise} A promise that resolves to an array of Course object: https://canvas.instructure.com/doc/api/courses.html
 */
 
 export default function getCoursesByUser (userId) {

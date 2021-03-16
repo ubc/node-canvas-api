6bf3310bb9d8b8a413ce5d354947bd75e1c635b9
Justin Lee <justin.lee@ubc.ca>
2021-03-16 15:05:57 -0700

    fixed typo

--- src/getFullGroupDiscussion.js
+++ src/getFullGroupDiscussion.js
@@ -3,7 +3,7 @@ var fetch = require('./internal/fetch');
 const canvasDomain = process.env.CANVAS_API_DOMAIN;
 
 /**
- * Retrives all discussion topics in course
+ * Retrives all discussion topics in group
  * @param {Number} groupId the group id.
  * @param {Number} topicId the discussion topic id.
  * @return {Promise} A promise that resolves to a discussion topic view object

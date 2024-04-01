1e0a57dad8706432c5312961e5304cb1f4aeb4aa
Abdelaziz <abel-haj@student.1337.ma>
2024-04-01 13:22:54 +0000

    Create a user in a specific account

---
+++ source/createUser.js
@@ -0,0 +1,17 @@
+import postRequest from './internal/post'
+
+require('dotenv').config()
+
+const canvasDomain = process.env.CANVAS_API_DOMAIN
+
+/**
+ * Creates group
+ * @param {Number} accountId the account that the user will be created in.
+ * @param {Number} body the object that contains the request parameters as indicated here:
+ * https://canvas.instructure.com/doc/api/users.html#method.users.create
+ * @return {Promise} A User object: https://canvas.instructure.com/doc/api/users.html#User
+ */
+
+export default function createUser (accountId, body) {
+  return postRequest(canvasDomain + `/accounts/${accountId}/users`, body)
+}
